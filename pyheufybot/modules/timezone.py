8487d76fc4fd4bd595f6055744e1bc45c3a210f7
Pobaxi <Fleischhacker.Christian@inode.at>
2014-05-22 21:12:13 +0200

    * python newb 101 mistake: dem tabs!
    * Added a bunch toughts on how to approach the first iteration of the feature.

--- pyheufybot/modules/timezone.py
+++ pyheufybot/modules/timezone.py
@@ -12,13 +12,22 @@ class ModuleSpawner(Module):
         self.moduleType = ModuleType.COMMAND
         self.modulePriority = ModulePriority.NORMAL
         self.messageTypes = ["PRIVMSG"]  #TODO EHHHH?
-        self.helpText = "Usage: timezone <from> <to> | Converts from one timezone to the other."
+        self.helpText = "Usage: timezone <HH:MM Timezone> <HH:MM Timezone> | Converts from one time to the other. " \
+                        "Timezone stands for the timezone shorthand, UTC for example."
 
-        def excecute(self, message):
-            #TODO
-            if len(message.params) == 1:
-                self.bot.msg(message.repyTo, "ehhh somethings wrong chap")
-            else:
-                self.bot.msg(message.repyTo, "Hey something went right chap!")
-                # Do the actual work of timezone converting?
+    def execute(self, message):
+        #TODO:
+        if len(message.params) == 1:
+            self.bot.msg(message.repyTo, "ehhh somethings wrong chap")
+        else:
+            self.bot.msg(message.repyTo, "Hey something went right chap!")
+            # Do the actual work of timezone converting?
+            # actual work:
+            #   check if <from> is something we can work with i.e: are you a time + known timezone
+            #   check if <to> is something we can work with i.e: are you a time + known timezone
+            #       sounds like a reusable function, challenge how to check? regex? good ol fashioned string
+            #       masturbation?
+            #   utilize something to make the actual conversion, python probably has something
+            #   return the conversion: challenge -> date change i.e: <to> is the next day, how to output the user?
+
         return True
